a46c007e7802ebf91d936bab394161e500e983a5
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-04-21 15:54:26 -0700

    Commiting with TedCruz animation varibales

--- main.js
+++ main.js
@@ -4,6 +4,10 @@
 
 var AM = new AssetManager();
 
+
+var CANVAS_HEIGHT = 626;
+var CANVAS_WIDTH = 1280;
+
 function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale){
     this.spriteSheet = spriteSheet;
     this.frameWidth = frameWidth;
@@ -33,11 +37,11 @@ Animation.prototype.drawFrame = function(tick, ctx, x, y) {
         x, y,
         this.frameWidth * this.scale,
         this.frameHeight * this.scale);
-}
+};
 
 Animation.prototype.currentFrame = function () {
     return Math.floor(this.elapsedTime / this.frameDuration);
-}
+};
 
 Animation.prototype.isDone = function () {
     return (this.elapsedTime >= this.totalTime);
@@ -60,34 +64,60 @@ Background.prototype.draw = function () {
 Background.prototype.update = function () {
 };
 
-function TedCruz(game, spriteSheet){
-    this.animation = new Animation();
-    this.x = ;
-    this.y = ;
-    this.speed = ;
-    this.game = game;
-    this.ctx = game.ctx;
+function TedCruz(game){
+    this.duckAnimation = new Animation(AM.getAsset("./img/Cruz/cruzDuck.png"), 202, 328, 5, 0.08, 20, false, 1);
+    this.jumpAnimation = new Animation(AM.getAsset("./img/Cruz/cruzJump.png"), 177, 244, 5, 0.08, 20, false, 1);
+    this.standingAnimation = new Animation(AM.getAsset("./img/Cruz/cruzStand.png"),140, 314, 5, 0.08, 6, true, 1);
+    this.punchAnimation = new Animation(AM.getAsset("./img/Cruz/cruzPunch.png"), 193, 242, 5, 0.08, 20, false, 1);
+    this.highKickAnimation = new Animation(AM.getAsset("./img/Cruz/cruzHighKick.png"), 375, 347, 5, 0.08, 20, false, 1);
+    this.lowKickAnimation = new Animation(AM.getAsset("./img/Cruz/cruzLowKick.png"), 334, 339, 5, 20, 0.08, 20, false, 1);
+    this.walkRightAnimation = new Animation(AM.getAsset("./img/Cruz/cruzWalkRight.png"), 153, 308, 5, 0.08, 20, true, 1);
+    this.walkLeftAnimation = new Animation(AM.getAsset("./img/Cruz/cruzWalkLeft.png"), 173, 314, 5, 0.08, 20, true, 1);
+
+    this.jumping = false;
+    this.punching = false;
+    this.ducking = false;
+    this.walkLeft = false;
+    this.walkRight = false;
+    this.standing = true;
+    this.lowKicking = false;
+    this.highKicking = false;
+
+    this.ground = (CANVAS_HEIGHT - 200);
+
+
+    // this.speed = ;
+    Entity.call(this, game, 0, (CANVAS_HEIGHT - 200));
+
 }
 
 TedCruz.prototype.draw = function(){
-
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+    Entity.prototype.draw.call(this);
 }
 
 TedCruz.prototype.update = function () {
-
+    if(this.game.space);
 }
 
-AM.queueDownload(insert Cruz sprite sheet);
+//download background image
+AM.queueDownload("./img/whiteHouse.jpg");
 
-AM downloadAll(function () {
+
+
+AM.downloadAll(function () {
     var canvas = document.getElementById("gameWorld");
     var ctx = canvas.getContext("2d");
     var gameEngine = new GameEngine();
+    var ted = new TedCruz(gameEngine);
+
+
+    //add entites to game
+    gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/whiteHouse.jpg")));
+
+
     gameEngine.init(ctx);
     gameEngine.start();
-
-    gameEngine.addEntity(new Background(gameEngine, AM.getAsset(insert background)));
-
     console.log("All Done!");
 });
 
