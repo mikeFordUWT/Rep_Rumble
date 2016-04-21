6f501382b3b2dbff7cfc68bfce201f73460340df
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-04-21 13:56:25 -0700

    some small changes

--- fighter.js
+++ fighter.js
@@ -1,3 +1,21 @@
 /**
  * Created by Mike on 4/21/16.
  */
+
+function Fighter(game, spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale){
+    this.animation = new Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale);
+    
+}
+
+function setX(){
+    
+}
+
+function setY(){
+    
+}
+
+function setCTX(){
+    
+}
+
--- main.js
+++ main.js
@@ -1,3 +1,93 @@
 /**
  * Created by Mike on 4/21/16.
  */
+
+var AM = new AssetManager();
+
+function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale){
+    this.spriteSheet = spriteSheet;
+    this.frameWidth = frameWidth;
+    this.frameHeight = frameHeight;
+    this.sheetWidth = sheetWidth;
+    this.frames = frames;
+    this.totalTime = frames * frameDuration;
+    this.loop = loop;
+    this.elapsedTime  = 0;
+    this.scale = scale;
+}
+
+Animation.prototype.drawFrame = function(tick, ctx, x, y) {
+    this.elapsedTime += tick;
+    if (this.isDone()) {
+        if (this.loop) this.elapsedTime = 0;
+    }
+    var frame = this.currentFrame();
+    var xindex = 0;
+    var yindex = 0;
+    xindex = frame % this.sheetWidth;
+    yindex = Math.floor(frame / this.sheetWidth);
+
+    ctx.drawImage(this.spriteSheet,
+        xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
+        this.frameWidth, this.frameHeight,
+        x, y,
+        this.frameWidth * this.scale,
+        this.frameHeight * this.scale);
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+// no inheritance
+function Background(game, spritesheet) {
+    this.x = 0;
+    this.y = 0;
+    this.spritesheet = spritesheet;
+    this.game = game;
+    this.ctx = game.ctx;
+};
+
+Background.prototype.draw = function () {
+    this.ctx.drawImage(this.spritesheet,
+        this.x, this.y);
+};
+
+Background.prototype.update = function () {
+};
+
+function TedCruz(game, spriteSheet){
+    this.animation = new Animation();
+    this.x = ;
+    this.y = ;
+    this.speed = ;
+    this.game = game;
+    this.ctx = game.ctx;
+}
+
+TedCruz.prototype.draw = function(){
+
+}
+
+TedCruz.prototype.update = function () {
+
+}
+
+AM.queueDownload(insert Cruz sprite sheet);
+
+AM downloadAll(function () {
+    var canvas = document.getElementById("gameWorld");
+    var ctx = canvas.getContext("2d");
+    var gameEngine = new GameEngine();
+    gameEngine.init(ctx);
+    gameEngine.start();
+
+    gameEngine.addEntity(new Background(gameEngine, AM.getAsset(insert background)));
+
+    console.log("All Done!");
+});
+
